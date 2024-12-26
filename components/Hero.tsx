b6ed8f8ac77e5e9bb989f0c5946cdc5f03794c9d
DarkInventor <ktmehta25@gmail.com>
2024-12-26 06:08:43 -0500

    fix hero copy

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -101,7 +101,7 @@ export default function Hero() {
           {/* <p className="pt-2 text-sm text-[#86868b] font-normal">No Credit Card Required</p>   */}
           <p className="absolute left-0 right-0 mx-auto mt-[8em] sm:mt-[10em] md:mt-[12em] max-w-[28em] bg-gradient-to-b from-[#86868b] to-[#bdc2c9] bg-clip-text text-center font-semibold text-transparent text-sm sm:text-base md:text-lg">
             Produce high impact{" "}
-             <span className="font-extrabold text-[#e7dfd6]">mockup ads</span>, <span className="font-extrabold text-[#e7dfd6]">social media posts</span>, <span className="font-extrabold text-[#e7dfd6]">product videos</span>, <span className="font-extrabold text-[#e7dfd6]">animated website demos</span>. Engage more audience — No Design Skils Required.
+             <span className="font-extrabold text-[#e7dfd6]">mockup ads</span>, <span className="font-extrabold text-[#e7dfd6]">social media posts</span>, <span className="font-extrabold text-[#e7dfd6]">product videos</span>, <span className="font-extrabold text-[#e7dfd6]">animated website demos</span>. Engage more audience — No Design Skills Required.
           </p>
         </div>
       </div>
